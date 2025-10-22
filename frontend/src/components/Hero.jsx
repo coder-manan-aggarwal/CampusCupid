f804220d6881a41efe06727b226fae10f0820a53
coder-manan-aggarwal <aggarwalmanan8@gmail.com>
2025-10-23 02:18:13 +0530

    fixed landing page issue

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
               muted
               playsInline
             >
-              <source src="src/assets/introVideo.mp4" type="video/mp4" />
+              <source src="/introVideo.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
